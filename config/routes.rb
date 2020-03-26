9184500ff243ee0122a322521e9fe12e64b5e6ba
waywho <walzerfan@yahoo.com>
2020-03-26 09:09:05 +0000

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'landings/index'
   root 'landings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20200326090838_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin
+      t.string :avatar
+
+      t.timestamps
+    end
+    add_index :users, :email
+    add_index :users, :admin
+  end
+end
