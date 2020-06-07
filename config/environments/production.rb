2a5715de79ec6b87c6c88eea28e1d0134ef09db0
waywho <walzerfan@yahoo.com>
2020-06-07 18:06:27 +0100

    add allow origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://parlour-games-who.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  config.action_cable.allowed_request_origins = [ 'http://parlourgames.club', /http:\/\/parlourgames.*/ ]
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
