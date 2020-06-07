cfed8add33f03928e8cce531a7b04b52f1904108
waywho <walzerfan@yahoo.com>
2020-06-07 15:44:59 +0100

    wss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -108,7 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	// ws://localhost:5000/
 
 	Vue.use(VueActionCable, {
-		connectionUrl: 'ws://parlour-games-who.herokuapp.com/cable'
+		connectionUrl: 'wss://parlourgames.club/cable'
 	})
 	
 	const app = new Vue({
