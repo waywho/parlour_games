b1298527e14793fa3eb47cc6ebb699f60243bddb
waywho <walzerfan@yahoo.com>
2020-06-07 18:17:49 +0100

    testing whitelist origin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://parlour-games-who.herokuapp.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/, /http:\/\/parlourgames.*/ ]
-  config.action_cable.allowed_request_origins = [ 'http://parlourgames.club', /http:\/\/*.parlourgames.club/ ]
+  config.action_cable.allowed_request_origins = [ 'http://parlourgames.club', 'http://www.parlourgames.club', /http:\/\/*.parlourgames.club/ ]
   
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
