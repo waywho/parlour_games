3caa5816f6153464bfc4cbc9d5b47f5d120cee34
waywho <walzerfan@yahoo.com>
2020-06-16 11:11:18 +0100

    add cookie

--- app/javascript/components/landing_page.vue
+++ app/javascript/components/landing_page.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="tile is-ancestor">
-    <div class="tile is-parent hero-tile">
+  <div class="tile is-ancestor is-vertical">
+    <div class="tile is-parent">
       <div class="tile is-child hero-tile">
         <h1 class="title is-1">Welcome to Parlour Games</h1>
       </div>
     </div>
+     <div class="tile is-parent">
+        <div class="tile is-chid">
+          <cookie-law theme="base"></cookie-law>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import CookieLaw from 'vue-cookie-law'
 export default {
+  components: { CookieLaw },
   data: function () {
     return {
       message: "Hello Vue !"
@@ -28,4 +35,6 @@ export default {
   align-items: center;
   min-height: 75vh;
 }
+
+
 </style>
